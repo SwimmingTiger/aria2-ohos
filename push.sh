03e29d299f2ac92263fc38ea8993c8c343e7f863
SwimmingTiger <hu60.cn@gmail.com>
2025-06-14 23:42:56 +0800

    deploy: push忘记加hap路径时给出提示

--- push.sh
+++ push.sh
@@ -3,9 +3,16 @@
 # assume deveco studio is downloaded from:
 # https://developer.huawei.com/consumer/cn/download/
 # and extracted to /Applications/DevEco-Studio.app
+
+if [ "$1" = "" ]; then
+    echo "Usage: $0 ./entry/build/default/outputs/default/entry-default-signed.hap"
+    exit 1
+fi
+
 set -x -e
 export TOOL_HOME=/Applications/DevEco-Studio.app/Contents
 export PATH=$TOOL_HOME/sdk/default/openharmony/toolchains:$PATH
+
 hdc file send $1 /data/local/tmp
 hdc shell bm uninstall -n $(jq ".app.bundleName" AppScope/app.json5)
 hdc shell bm install -p /data/local/tmp/$(basename $1)
